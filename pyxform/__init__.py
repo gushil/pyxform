26ad76fb6b504725179362aa78fa0147681293dd
Shiva Vadla <svadla@openclinica.com>
2023-03-02 11:02:34 -0500

    Updating version info for new release

--- pyxform/__init__.py
+++ pyxform/__init__.py
@@ -4,7 +4,7 @@ pyxform is a Python library designed to make authoring XForms for ODK
 Collect easy.
 """
 
-__version__ = "1.10.1"
+__version__ = "oc-1.0.0"
 
 from pyxform.builder import (
     SurveyElementBuilder,
